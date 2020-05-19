8475c4a4255f0af7988f09305797d09fed4c5df6
aswa09 <62519648+aswa09@users.noreply.github.com>
2020-05-19 14:59:00 +0530

    Add files via upload

---
+++ S7/visualise_loss_acc.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import sys
+
+##Plotting Test Accuracies and Test Losses
+
+def plot_acc_loss(test_losses, test_acc):
+  try:
+  
+    fig, (ax1, ax2) = plt.subplots(2, figsize=(15,10))
+    fig.suptitle('Test Loss and Test Accuracy for the models', fontsize=16)
+    ax1.plot(test_losses[0])
+
+    ax1.set_title("Test Loss")
+    
+    ax2.plot(test_acc[0])
+
+    ax2.set_title("Test Accuracy")
+   
+    plt.savefig('acc_vs_loss.jpg')
+    plt.show()
+  except Exception as e:
+        print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno) + " " + type(e).__name__ + " " + str(e))
+        sys.exit(1)
