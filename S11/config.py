a8b5fdf04d1ad6905da3a0868b3da310c7d136aa
aswa09 <62519648+aswa09@users.noreply.github.com>
2020-06-14 06:22:50 +0530

    Add files via upload

---
+++ S11/config.py
@@ -0,0 +1,12 @@
+args={
+	'classes':['plane', 'car', 'bird', 'cat', 'deer', 'dog','frog', 'horse', 'ship', 'truck'],
+	'lrate':0.001,
+	'momentum':0.9,
+	'batch_size':512,
+	'EPOCHS':24,
+	'l1':2e-6,
+	'l2':6e-4,
+	'end_lr':0.02,
+	'lr_epochs':50,
+	'step_mode':'linear'
+}
