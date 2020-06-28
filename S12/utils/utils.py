331e7fdc1dbb3344aff45f41ea99d155cbfaa507
aswa09 <62519648+aswa09@users.noreply.github.com>
2020-06-28 08:08:24 +0530

    Add files via upload

---
+++ S12/utils/utils.py
@@ -0,0 +1,41 @@
+import torch
+import numpy as np
+
+
+def unnormalize(image, mean, std, transpose=False):
+    """Un-normalize a given image.
+
+    Args:
+        image (numpy.ndarray or torch.Tensor): A ndarray
+            or tensor. If tensor, it should be in CPU.
+        mean (float or tuple): Mean. It can be a single value or
+            a tuple with 3 values (one for each channel).
+        std (float or tuple): Standard deviation. It can be a single
+            value or a tuple with 3 values (one for each channel).
+        transpose (bool, optional): If True, transposed output will
+            be returned. This param is effective only when image is
+            a tensor. If tensor, the output will have channel number
+            as the last dim. (default: False)
+
+    Returns:
+        Unnormalized image
+    """
+
+    # Check if image is tensor, convert to numpy array
+    tensor = False
+    if type(image) == torch.Tensor:  # tensor
+        tensor = True
+        if len(image.size()) == 3:
+            image = image.transpose(0, 1).transpose(1, 2)
+        image = np.array(image)
+
+    # Perform normalization
+    image = image * std + mean
+
+    # Convert image back to its original data type
+    if tensor:
+        if not transpose and len(image.shape) == 3:
+            image = np.transpose(image, (2, 0, 1))
+        image = torch.Tensor(image)
+
+    return image
